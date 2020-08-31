4323be39364418e8059b7e3f3e774676ec4a356e
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2020-08-31 21:27:28 +0200

    New project added

---
+++ Uppgift-2/kenkata/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    
+    <navbar />
+
+    <router-view/>
+
+  </div>
+
+</template>
+
+<script>
+
+import navbar from '@/components/navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navbar,
+  }
+}
+</script>
+
+
+<style>
+
+  :root {
+    --ownOrange: #FFBB00;
+    --ownBlue: #20D3C2; 
+    --btnHover: #098d80; 
+    --ownGray: #333333;
+    --ownLightGray: #8A8A8A;  
+  }
+
+  .my-blue-color {
+    color: var(--ownBlue);
+  }
+  
+  .my-blue {
+    background-color: #20D3C2;
+  }
+
+  * {
+    font-family: 'Roboto', sans-serif;
+    /* padding: 0 !important; */
+  }
+
+  .Roboto {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .WHITE-BTN {
+    background-color: white;
+    border-radius: 30px;
+    padding: 8px 20px 8px 20px;
+  }
+
+
+</style>
---
+++ Uppgift-2/kenkata/src/views/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="my-relative">
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="@/assets/HomeBG.png" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="@/assets/HomeBG.png" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="@/assets/HomeBG.png" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <i class="fas fa-chevron-left arrow-btns"></i>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <i class="fas fa-chevron-right arrow-btns"></i>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <div class="covertext container">
+      <div class="">
+        <h1 class="title">Shopping is</h1>
+        <h1 class="title my-blue-color font-weight-bold">MORE FUN</h1>
+        <p class="subtitle">Vivamus dignissim a ex eu ornare. Ut fermentum orci <br>
+        vel diam ultricies faucibus. Quisque quis erat sed</p>
+      </div>
+      <div class="">
+        <a href="#"><button type="button" class="btn btn-lg WHITE-BTN font-weight-bold my-blue-color">SHOP NOW</button></a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+  .my-relative {
+    position: relative;
+  }
+
+  .covertext {
+    position: absolute; /* To make the div to be place anywhere. It is out of the document flow */
+    top: 120px; /* The distance between the div with the top of document */
+    left: 0px; /* Make the div full width */
+    right: 0px; /* Make the div full width */
+  }
+
+  .title {
+    font-size: 60px;
+  }
+
+  .subtitle {
+
+  }
+
+
+  .arrow-btns {
+    font-size: 30px;
+    color: var(--ownBlue);
+  }
+
+  /* .first-section {
+    position: absolute;
+    right: 10%;
+    bottom: 250px;
+    left: 400px;
+    z-index: 10;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    color: #fff;
+    text-align: left;
+  } */
+
+</style>
